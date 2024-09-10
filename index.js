3ccd59b7d5b0c08951f03798d3cba85772a2976b
NotTimTam <timothyrp6@gmail.com>
2024-09-09 20:35:18 -0700

    Bugfix.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import ImageModules from "./modules/images";
+import ImageModules from "./modules/images.js";
 
-export { default as ImageModules } from "./modules/images";
+export { default as ImageModules } from "./modules/images.js";
 
 const CoreModules = [...ImageModules];
 
