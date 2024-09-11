ffd5e15730e8b36ae338f73cd73db9d23c62d54e
NotTimTam <timothyrp6@gmail.com>
2024-09-10 21:08:47 -0700

    Implemented more module options.

--- modules/images.js
+++ modules/images.js
@@ -15,7 +15,6 @@ const ImageModules = [
 					"Use progressive (interlace) scan. (default false)",
 				type: "boolean",
 				default: false,
-				required: true,
 				validateInput: async (value) => {
 					if (Boolean(value) !== value)
 						throw new SyntaxError(
@@ -54,11 +53,93 @@ const ImageModules = [
 				description: "Use adaptive row filtering.",
 				type: "boolean",
 				default: false,
-				required: false,
 				validateInput: async (value) => {
 					if (Boolean(value) !== value)
 						throw new SyntaxError(
 							`AdaptiveFiltering should be a boolean.`
+						);
+				},
+			}),
+			new Module.Option({
+				label: "Palette",
+				description:
+					"Quantise to a palette-based image with alpha transparency support",
+				type: "boolean",
+				default: false,
+				validateInput: async (value) => {
+					if (Boolean(value) !== value)
+						throw new SyntaxError(`Palette should be a boolean.`);
+				},
+			}),
+			new Module.Option({
+				label: "Quality",
+				description:
+					"Use the lowest number of colours needed to achieve given quality, sets palette to true.",
+				type: "number",
+				default: 100,
+				required: true,
+				validateInput: async (value) => {
+					if (typeof value !== "number")
+						throw new SyntaxError(`Quality should be a number.`);
+					if (value < 0)
+						throw new SyntaxError(
+							`Quality should be greater than or equal to 0.`
+						);
+					if (value > 100)
+						throw new SyntaxError(
+							`Quality should be less than or equal to 100.`
+						);
+					if (value % 1 !== 0)
+						throw new SyntaxError(
+							`Quality should be an integer between 0 and 100. (inclusive)`
+						);
+				},
+			}),
+			new Module.Option({
+				label: "Effort",
+				description:
+					"CPU effort, between 1 (fastest) and 10 (slowest), sets palette to true",
+				type: "number",
+				default: 7,
+				required: true,
+				validateInput: async (value) => {
+					if (typeof value !== "number")
+						throw new SyntaxError(`Quality should be a number.`);
+					if (value < 1)
+						throw new SyntaxError(
+							`Quality should be greater than or equal to 1.`
+						);
+					if (value > 10)
+						throw new SyntaxError(
+							`Quality should be less than or equal to 10.`
+						);
+					if (value % 1 !== 0)
+						throw new SyntaxError(
+							`Quality should be an integer between 0 and 10. (inclusive)`
+						);
+				},
+			}),
+			new Module.Option({
+				label: "Colors",
+				description:
+					"Maximum number of palette entries, sets palette to true",
+				type: "number",
+				default: 256,
+				required: true,
+				validateInput: async (value) => {
+					if (typeof value !== "number")
+						throw new SyntaxError(`Quality should be a number.`);
+					if (value < 1)
+						throw new SyntaxError(
+							`Quality should be greater than or equal to 1.`
+						);
+					if (value > 256)
+						throw new SyntaxError(
+							`Quality should be less than or equal to 256.`
+						);
+					if (value % 1 !== 0)
+						throw new SyntaxError(
+							`Quality should be an integer between 1 and 256. (inclusive)`
 						);
 				},
 			}),
@@ -70,10 +151,12 @@ const ImageModules = [
 			await sharp
 				.png({
 					progressive: options.ProgressiveInterlacing,
-					compressionLevel: options.CompressionLevel
-						? options.CompressionLevel
-						: 6,
+					compressionLevel: options.CompressionLevel,
 					adaptiveFiltering: options.AdaptiveFiltering,
+					palette: options.Palette,
+					quality: options.Quality,
+					effort: options.Effort,
+					colors: options.Colors,
 				})
 				.toFile(path);
 		},
