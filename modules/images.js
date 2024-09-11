44fdb044cfb71c935537930da5a6298a1c5016b7
NotTimTam <timothyrp6@gmail.com>
2024-09-10 17:38:57 -0700

    Implemented module options.

--- modules/images.js
+++ modules/images.js
@@ -8,7 +8,35 @@ const ImageModules = [
 		description: "Convert .jpeg files to .png.",
 		from: "image/jpeg",
 		to: "image/png",
-		method: async ({ path }) => {
+		options: [
+			new Module.Option({
+				label: "CompressionLevel",
+				description:
+					"zlib compression level, 0 (fastest, largest) to 9 (slowest, smallest). (default 6)",
+				type: "number",
+				validateInput: async (value) => {
+					if (typeof value !== "number")
+						throw new SyntaxError(
+							`CompressionLevel should be a number.`
+						);
+					if (value < 0)
+						throw new SyntaxError(
+							`CompressionLevel should be greater than or equal to 0.`
+						);
+					if (value > 9)
+						throw new SyntaxError(
+							`CompressionLevel should be less than or equal to 9.`
+						);
+					if (value % 1 !== 0)
+						throw new SyntaxError(
+							`CompressionLevel should be an integer between 0 and 9. (inclusive)`
+						);
+				},
+			}),
+		],
+		method: async ({ path }, options) => {
+			console.log(options);
+
 			const data = await fs.readFile(path);
 
 			const sharp = new Sharp(data);
