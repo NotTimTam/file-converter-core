b4990642425fe29c7f997455b41ba2811948658b
NotTimTam <timothyrp6@gmail.com>
2024-09-12 16:07:26 -0700

    Added PDFModule imports.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import ImageModules from "./modules/images.js";
+import PDFModules from "./modules/pdfs.js";
 
 export { default as ImageModules } from "./modules/images.js";
+export { default as PDFModules } from "./modules/pdfs.js";
 
-const CoreModules = [...ImageModules];
+const CoreModules = [...ImageModules, ...PDFModules];
 
 export default CoreModules;
