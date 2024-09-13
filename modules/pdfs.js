0e613214b1f48eab50dbb7c71669ef9730f35c0d
NotTimTam <timothyrp6@gmail.com>
2024-09-12 17:02:24 -0700

    Upgraded PDF module.

--- modules/pdfs.js
+++ modules/pdfs.js
@@ -1,21 +1,25 @@
 import { Module } from "@nottimtam/file-converter";
 import fs from "fs-extra";
 import PDFDocument from "pdfkit";
+import sizeOf from "image-size";
 
 const PDFModules = [
 	new Module({
-		label: "JPEGToPDF",
-		description: "Convert .jpeg files to .pdf.",
-		from: "image/jpeg",
+		label: "ImageToPDF",
+		description: "Convert .jpeg and .png files to .pdf.",
+		from: ["image/jpeg", "image/png"],
 		to: "application/pdf",
 		options: [],
 		method: async ({ path }, options = {}) => {
 			const data = await fs.readFile(path);
 
-			// Create PDF.
-			const pdf = new PDFDocument();
+			// Get image dimensions.
+			const { width, height } = sizeOf(path);
 
-			pdf.image(path);
+			// Create PDF.
+			const pdf = new PDFDocument({ size: [width, height], margin: 0 });
+
+			pdf.image(path, 0, 0, { width, height });
 
 			// Save data.
 			pdf.pipe(fs.createWriteStream(path));
