d36d11fa68bb8fcf9d00d1e9ced6cb60a06e4289
NotTimTam <timothyrp6@gmail.com>
2024-09-12 19:56:43 -0700

    Bugfixes and removal of incompatible modules.

--- modules/documents.js
+++ modules/documents.js
@@ -2,7 +2,6 @@ import { Module } from "@nottimtam/file-converter";
 import fs from "fs-extra";
 import sizeOf from "image-size";
 import PDFDocument from "pdfkit"; // Generates PDFs.
-import Poppler from "node-poppler"; // Generates HTML and TXT from PDFs.
 import { Document, Packer, Paragraph, TextRun } from "docx"; // Generates DOCX files.
 import mammoth from "mammoth"; // Reads DOCX files.
 
@@ -26,58 +25,6 @@ const DocumentModules = [
 
 			// Close process.
 			pdf.end();
-		},
-	}),
-	new Module({
-		label: "PDFtoTXT",
-		description: "Convert .pdf files to .txt.",
-		from: "application/pdf",
-		to: "text/plain",
-		method: async ({ path }) => {
-			const buffer = await fs.readFile(path);
-
-			await new Poppler().pdfToText(buffer, path);
-		},
-	}),
-	new Module({
-		label: "PDFtoHTML",
-		description: "Convert .pdf files to .html.",
-		from: "application/pdf",
-		to: "text/html",
-		method: async ({ path }) => {
-			const buffer = await fs.readFile(path);
-
-			await new Poppler().pdfToHtml(buffer, path);
-		},
-	}),
-	new Module({
-		label: "PDFtoDOCX",
-		description: "Convert .pdf files to .txt.",
-		from: "application/pdf",
-		to: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-		method: async ({ path }) => {
-			const buffer = await fs.readFile(path);
-			await new Poppler().pdfToText(buffer, path); // Write text to file.
-
-			// Create a new DOCX document
-			const doc = new Document({
-				sections: [
-					{
-						properties: {},
-						children: [
-							new Paragraph({
-								children: [
-									new TextRun(await fs.readFile(path)),
-								],
-							}),
-						],
-					},
-				],
-			});
-
-			// Write the DOCX file
-			const docxBuffer = await Packer.toBuffer(doc);
-			await fs.writeFile(path, docxBuffer);
 		},
 	}),
 	new Module({
@@ -108,11 +55,13 @@ const DocumentModules = [
 			const text = await fs.readFile(path, "utf-8");
 
 			// Create a new DOCX document
-			const doc = new Document();
-
-			// Add text to the DOCX document
-			doc.addSection({
-				children: [new Paragraph(text)],
+			const doc = new Document({
+				sections: [
+					{
+						properties: {},
+						children: [new Paragraph({ text })],
+					},
+				],
 			});
 
 			// Create a buffer with the DOCX data
